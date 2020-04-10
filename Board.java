c210a852e7a3aee8e81a591c8ef6eb0aab985669
Hayley Gelder <63299952+HGelder@users.noreply.github.com>
2020-04-10 17:40:16 +0100

    Fixed null pointer issue
    Board now prints correctly again with new Node-type tiles.

--- Board.java
+++ Board.java
@@ -1,4 +1,4 @@
-public class Board {
+public class Board { 
 
     public final String space = "[ ]";
     public final int maxRows;
@@ -9,6 +9,12 @@ public class Board {
         this.maxRows = maxRows;
         this.maxCols = maxCols;
         this.boardTiles = new Node[maxRows][maxCols];
+
+        for (int row = 0; row < maxRows; row++) {
+            for (int col = 0; col < maxCols; col++) {
+                boardTiles[row][col] = new Node(row, col, Node.NodeState.WALKABLE, "[ ]");
+            }
+        }
     }
 
     public Node getTile (int rowNumber, int colNumber) {
