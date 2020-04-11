2944d854593fcea2a6520fd46449488a020edb69
Hayley Gelder <63299952+HGelder@users.noreply.github.com>
2020-04-11 16:31:12 +0100

    Added methods for list management
    Added several methods; nodes are now added/removed from open/closed lists as needed and able to get neighbouring nodes. 
    (Note: method to get neighbouring nodes is borrowed completely for now, will be rewritten originally to fit in next edit.)

--- Main.java
+++ Main.java
@@ -1,13 +1,20 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.Scanner; 
+import java.util.Scanner;
+import java.util.List;
 
 public class Main {
 
     public static Board gameBoard;
+    public static Node currentNode;
+    public static Node startNode; 
+    public static Node endNode; 
+    public static Node[][] boardTiles; 
+
     private static Scanner userInput;
-    private static int[] input = {0, 0};
     private static int boardSize = 5;
+    private static int[] input = {0, 0};
+    private static List<Node> tempNeighbours; 
 
     public static void main (String args[]) {
         newGame(boardSize, boardSize);
@@ -16,6 +23,20 @@ public class Main {
         placeObject(getObjectCell("Wall"), "Wall");
         placeObject(getObjectCell("Wall"), "Wall");
         placeObject(getObjectCell("Wall"), "Wall");
+
+        boardTiles = gameBoard.getTiles();
+        AI ai = new AI (startNode, endNode, gameBoard, boardTiles);
+
+        currentNode = startNode;
+        tempNeighbours = ai.getNeighbours(startNode, boardTiles);
+        
+        for (int i = 0; i < tempNeighbours.size(); i++) {
+            ai.addToList("Open", tempNeighbours.get(i));
+            tempNeighbours.get(i).setParent(currentNode);
+        }
+
+        ai.removeFromList("Open", currentNode);
+        ai.addToList("Closed", currentNode);
     }
 
     public static void newGame (int maxRows, int maxCols) {
@@ -30,19 +51,19 @@ public class Main {
         
         if (object.equals("Player")) {
 
-            System.out.print("Enter row number in which to place player: ");
+            System.out.print("Enter row number (1-5) in which to place player: ");
             input[0] = userInput.nextInt();
             
-            while (input[0] < 0 || input[0] > 4) {
-                System.out.println("Please enter a number between 0 and 4");
+            while (input[0] < 1 || input[0] > 5) {
+                System.out.println("Please enter a number between 1 and 5");
                 input[0] = userInput.nextInt();
             }
 
-            System.out.print("Enter col number in which to place player: ");
+            System.out.print("Enter col number (1-5) in which to place player: ");
             input[1] = userInput.nextInt();
 
-            while (input[1] < 0 || input[1] > 4) {
-                System.out.println("Please enter a number between 0 and 4");
+            while (input[1] < 1 || input[1] > 5) {
+                System.out.println("Please enter a number between 1 and 5");
                 input[1] = userInput.nextInt();
             }
 
@@ -50,19 +71,19 @@ public class Main {
 
         } else if (object.equals("Enemy")) {
 
-            System.out.print("Enter row number in which to place enemy: ");
+            System.out.print("Enter row number (1-5) in which to place enemy: ");
             input[0] = userInput.nextInt();
 
-            while (input[0] < 0 || input[0] > 4) {
-                System.out.println("Please enter a number between 0 and 4");
+            while (input[0] < 1 || input[0] > 5) {
+                System.out.println("Please enter a number between 1 and 5");
                 input[0] = userInput.nextInt();
             }
 
-            System.out.print("Enter col number in which to place enemy: "); 
+            System.out.print("Enter col number (1-5) in which to place enemy: "); 
             input[1] = userInput.nextInt();
 
-            while (input[1] < 0 || input[1] > 4) {
-                System.out.println("Please enter a number between 0 and 4");
+            while (input[1] < 1 || input[1] > 5) {
+                System.out.println("Please enter a number between 1 and 5");
                 input[1] = userInput.nextInt();
             }
 
@@ -70,19 +91,19 @@ public class Main {
 
         } else if (object.equals("Wall")) {
 
-            System.out.print("Enter row number in which to place wall: ");
+            System.out.print("Enter row number (1-5) in which to place wall: ");
             input[0] = userInput.nextInt();
 
-            while (input[0] < 0 || input[0] > 4) {
-                System.out.println("Please enter a number between 0 and 4");
+            while (input[0] < 1 || input[0] > 5) {
+                System.out.println("Please enter a number between 1 and 5");
                 input[0] = userInput.nextInt();
             }
 
-            System.out.print("Enter col number in which to place wall: ");
+            System.out.print("Enter col number (1-5) in which to place wall: ");
             input[1] = userInput.nextInt();
 
-            while (input[1] < 0 || input[1] > 4) {
-                System.out.println("Please enter a number between 0 and 4");
+            while (input[1] < 1 || input[1] > 5) {
+                System.out.println("Please enter a number between 1 and 5");
                 input[1] = userInput.nextInt();
             }
 
@@ -93,14 +114,16 @@ public class Main {
 
     public static void placeObject (int[] cell, String object) {
 
-        int rowNum = (int)Array.get(cell, 0);
-        int colNum = (int)Array.get(cell, 1);
+        int rowNum = (int)Array.get(cell, 0) - 1;
+        int colNum = (int)Array.get(cell, 1) - 1;
 
         String tileContents = gameBoard.getTile(rowNum, colNum).getContents();
+        Node.NodeState state = gameBoard.getTile(rowNum, colNum).getState(); 
 
         if (object.equals("Player")) {
 
             if (tileContents.equals("[ ]")) {
+                endNode = new Node(rowNum, colNum, Node.NodeState.WALKABLE, "[P]"); 
                 gameBoard.setTile(rowNum, colNum, "[P]");
                 gameBoard.print();
             } else {
@@ -111,6 +134,7 @@ public class Main {
         } else if (object.equals("Enemy")) {
             
             if (tileContents.equals("[ ]")) {
+                startNode = new Node(rowNum, colNum, Node.NodeState.WALKABLE, "[E]"); 
                 gameBoard.setTile(rowNum, colNum, "[E]");
                 gameBoard.print();
             } else {
@@ -121,6 +145,7 @@ public class Main {
         } else if (object.equals("Wall")) {
 
             if (tileContents.equals("[ ]")) {
+                state = Node.NodeState.UNWALKABLE; 
                 gameBoard.setTile(rowNum, colNum, "[X]");
                 gameBoard.print();
             } else {
